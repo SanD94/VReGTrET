a197285eb1b95b74a79b47c5d0ebc3aa8a37f9be
Safa Andac <8544658+SanD94@users.noreply.github.com>
2024-07-08 09:13:01 +0000

    adds graph with border option

---
+++ GraphTheory_ET_VR_Westbrueck/Pre-processing/add_loc_graph.m
@@ -0,0 +1,12 @@
+function loc_table = add_loc_graph(node_table, restrict_coord_file)
+    arguments (Input)
+        node_table table
+        restrict_coord_file (1,1) string = fullfile("utils", "restricted_area_coord_hera.txt")
+    end
+
+    pos = readmatrix(restrict_coord_file);
+    xq = node_table.x;
+    yq = node_table.y;
+    loc_table = node_table;
+    loc_table.inside = inpolygon(xq, yq, pos(:,1), pos(:,2));
+end
---
+++ GraphTheory_ET_VR_Westbrueck/Pre-processing/get_full_graph.m
@@ -0,0 +1,30 @@
+function f_graph = get_full_graph(collider_list_file, is_loc)
+    arguments (Input)
+        collider_list_file (1,1) string = fullfile("utils", "building_collider_list.csv")
+        is_loc (1,1) { mustBeNumericOrLogical } = false
+    end
+
+    arguments (Output)
+        f_graph (1,1) graph
+    end
+
+    collider_list = readtable(collider_list_file);
+    node_table = collider_list(:, {'Var1', 'target_collider_name', ...
+        'transformed_collidercenter_x', 'transformed_collidercenter_y'});
+
+    % Name is the convention for graph
+    node_table = renamevars(node_table, ...
+        ["Var1", "target_collider_name", ...
+            "transformed_collidercenter_x", ...
+            "transformed_collidercenter_y"], ...
+        ["ID", "Name", "x", "y"]);
+    [C, ia] = unique(node_table.Name);
+    node_table = node_table(ia, :);
+    if (is_loc)
+        node_table = add_loc_graph(node_table);
+    end
+
+    f_graph = graph(zeros(height(node_table)), node_table);
+end
+
+% Token 

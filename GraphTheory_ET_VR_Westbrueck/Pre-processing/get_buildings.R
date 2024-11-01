d5fbc9196b170840751ea6599e88c75d799429e0
Safa Andac <8544658+SanD94@users.noreply.github.com>
2024-11-01 11:46:12 +0100

    add building matrix

--- GraphTheory_ET_VR_Westbrueck/Pre-processing/get_buildings.R
+++ GraphTheory_ET_VR_Westbrueck/Pre-processing/get_buildings.R
@@ -17,19 +17,20 @@ get_buildings <- function(file_path = "../additional_Files/building_collider_lis
     buildings
 }
 
-get_building_counter <- function() {
+get_building_counter <- function(def_val = 0) {
     buildings <- get_buildings()
     building_counter <- buildings %>% pull(Name) %>% reduce(function (acc, cur) {
-            acc[[cur]] <- 0
+            acc[[cur]] <- def_val
             acc
         }, .init = list())
     # noData and newSession should be interpreted as buildings for the analysis before generating the graph
-    building_counter[["noData"]] <- 0
-    building_counter[["newSession"]] <- 0
+    building_counter[["noData"]] <- def_val
+    building_counter[["newSession"]] <- def_val
     building_counter
 }
 
 get_adj_list <- function(building_data, include_noisy = FALSE) {
+    initial_edges <- get_building_counter <- 
     if (!include_noisy)
         building_data <- building_data %>% filter(noisy == FALSE)
     order_buildings <- building_data %>% pull(building) %>% as.character()

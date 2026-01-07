059f500e945fb45069501ece33fe79b518604847
Safa Andac <8544658+SanD94@users.noreply.github.com>
2026-01-07 14:17:31 +0100

    remove redundancy for paper and move fun to playground

--- GraphTheory_ET_VR_Westbrueck/Analysis/raw_gaze_analysis/init/init_library.R
+++ GraphTheory_ET_VR_Westbrueck/Analysis/raw_gaze_analysis/init/init_library.R
@@ -6,11 +6,17 @@ init_library <- function() {
     library(broom.mixed)
     library(gt)
     library(ggprism)
-    library(marginaleffects)
-    library(ggeffects)
+    library(patchwork)
     library(scales)
     library(cli)
     library(here)
+
+    # stat analysis libraries
+    library(marginaleffects)
+    library(ggeffects)
+    library(lme4)
+    library(emmeans)
+    library(performance)
 
     cli_alert_success("Libraries are loaded")
 }
